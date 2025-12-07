660aaa81805dc9187906128ae2d1d37066ea2b2b
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-12-08 01:17:31 +0530

    Written the logic for the theme switcher

--- themeSwitcher/src/context/theme.js
+++ themeSwitcher/src/context/theme.js
@@ -1,9 +1,15 @@
 import { createContext, useContext } from "react";
 
-export default ThemeContext = createContext({
+export const ThemeContext = createContext({
     themeMode: "light",
     darkTheme: () => {},
     lightTheme: () => {},
 })
+
+export const ThemeProvider = ThemeContext.Provider
+
+export default function useTheme() {
+    return useContext(ThemeContext)
+}
 
 // You can also pass variable and methods inside Context.

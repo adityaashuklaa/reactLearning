af9ab2af9c8d7d33ff8bf1ab9c079d0862f6bfd5
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-12-08 01:51:46 +0530

    Completed ThemeSwitcher

--- themeSwitcher/src/App.jsx
+++ themeSwitcher/src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { ThemeProvider } from "./context/theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
 
 function App() {
   const [themeMode, setThemeMode] = useState("light")
@@ -20,13 +22,15 @@ function App() {
 
   return (
     <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
-      <div className="flex flex-wrap min-h-screen items-center">
+      <div className="flex flex-wrap min-h-screen items-center justify-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
             {/* Theme Btn */}
+            <ThemeBtn />
           </div>
           <div className="w-full max-w-sm mx-auto">
             {/* Card  */}
+            <Card />
           </div>
         </div>
       </div>

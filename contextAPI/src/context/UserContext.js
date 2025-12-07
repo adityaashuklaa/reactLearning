57f061b057c1052217277c74bf02969e917ecc7e
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-12-08 00:36:24 +0530

    UserContext

---
+++ contextAPI/src/context/UserContext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserContext = React.createContext()
+
+export default UserContext;
+
+// Context provides PROVIDER. It will be a wrapper, and every children will have the access of it's contents[STATES].

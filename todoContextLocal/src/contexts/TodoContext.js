23f0b65fc9332f982cd9be481f00d299ea4aaa9e
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2025-12-13 15:54:48 +0530

    Created Context and method function for adding, updating and deleting todo

--- todoContextLocal/src/contexts/TodoContext.js
+++ todoContextLocal/src/contexts/TodoContext.js
@@ -1,6 +1,18 @@
 import { createContext, useContext } from "react";
 
-export const TodoContext = createContext({})
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: "todo msg",
+            completed: false,
+        }
+    ],
+    addTodo: (todo) => {},
+    updatedTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
 
 export const useTodo = () => {
     return useContext(TodoContext) // Whenever useContext is used, you need to pass on the context.

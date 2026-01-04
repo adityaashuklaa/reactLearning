91eb0110e5b24d4dac156cf91f4fa3c1e4b8892f
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2026-01-05 02:33:13 +0530

    configured state, store

--- megaBlog/src/App.jsx
+++ megaBlog/src/App.jsx
@@ -1,13 +1,33 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Header, Footer } from './components'
+import { Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  console.log(); // To access env
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
   
-  return (
-    <>
-      <h1>A blog app with appwrite</h1>
-    </>
-  )
+  return !loading ? (
+    <div className=''>Heloooo</div> 
+  ) : (null)
 }
 
 export default App
+
+// First we'll create a loading state, which is a good practice to follow, as whenever you're making any network call at that particular moment you can do conditional rendering, using the loading state.
+// useDispatch is a merger which combines redux with react

36d7e459538e1e3484ae840e9bbbd2c57a9703d5
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2026-01-05 00:53:06 +0530

    Created Complete Appwrite Services for Creating account, login, logout, and getting current user

--- megaBlog/src/appwrite/auth.js
+++ megaBlog/src/appwrite/auth.js
@@ -33,6 +33,25 @@ export class AuthService {
             throw error
         }
     }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: user", error);
+        }
+
+        return null;
+    }
+
+    async logout() {
+        try {
+            return await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite service :: logout :: error", error);
+            
+        }
+    }
 }
 
 const authService = new AuthService();

9bc3b00db9b13fa94afeb297edebceded8c5bb6c
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2026-01-05 01:35:58 +0530

    Created Services for creating, updating and deleting Blogs in buckets

--- megaBlog/src/appwrite/conf.js
+++ megaBlog/src/appwrite/conf.js
@@ -1,2 +1,65 @@
 import config from "../config/config";
-import { Client, Account, ID } from "appwrite";
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+export class Service{
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor(){
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+        this.account = new Account(this.client);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+
+    async createPost({title, slug, content, featuredImage, status, userId}) {
+        try {
+            return await this.databases.createDocument(config.appwriteDatabaseId, config.appwriteCollectionId, slug, {
+                title,
+                content,
+                featuredImage,
+                status,
+                userId
+            })
+        } catch (error) {
+            console.log("Appwrite service :: createPost :: error", error);
+        }
+    }
+
+    async updatePost(slug, {title, content, featuredImage, status}) {
+        try {
+            return await this.databases.updateDocument(config.appwriteDatabaseId, config.appwriteCollectionId, slug, {
+                title,
+                content,
+                featuredImage,
+                status,
+            })
+        } catch (error) {
+            console.log("Appwrite service :: updatePost :: error", error);
+        }
+    }
+
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            )
+            return true;
+        } catch (error) {
+            console.log("Appwrite service :: deletePost :: error", error);
+            return false;
+        }
+    }
+}
+
+const service = new Service();
+
+export default service;
+
+// We've created service using new method, just to create object out of the service so that we can extract values from the object. We've created service using NEW method, so we've to also create constructor for it. 
+// Account should be created when the constructor is being called.

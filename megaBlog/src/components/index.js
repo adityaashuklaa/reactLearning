36830ce6d83c2d1607efe2041f83324567a3f3d1
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2026-01-09 01:45:29 +0530

    Updated index

--- megaBlog/src/components/index.js
+++ megaBlog/src/components/index.js
@@ -1,4 +1,11 @@
+import Container from "./container/Container";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import Logo from "./Logo";
 
-export {Header, Footer}
+export {
+    Header,
+    Footer,
+    Container, 
+    Logo
+}

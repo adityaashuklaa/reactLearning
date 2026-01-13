daf5071fb098cb71fc8d70b6d2f44deb9e0e1261
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2026-01-14 00:05:51 +0530

    forwardRef

--- megaBlog/src/components/index.js
+++ megaBlog/src/components/index.js
@@ -2,10 +2,12 @@ import Container from "./container/Container";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import Logo from "./Logo";
+import Button from "./Button";
 
 export {
     Header,
     Footer,
     Container, 
-    Logo
+    Logo,
+    Button
 }

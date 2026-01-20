1b0af08f559d7b7676d6b2930697cab6e0ebc3db
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2026-01-21 00:38:48 +0530

    Module and common js

--- megaBlog/src/App.jsx
+++ megaBlog/src/App.jsx
@@ -39,3 +39,7 @@ export default App
 
 // First we'll create a loading state, which is a good practice to follow, as whenever you're making any network call at that particular moment you can do conditional rendering, using the loading state.
 // useDispatch is a merger which combines redux with react
+
+// There are two ways to import any component in react application, common.js and module js. 
+// using import is a type of module js import which is asynchonorous in nature, and the other method is require, which is synchonorous in nature.
+// require syntax is used for common js, and import is used for module js.

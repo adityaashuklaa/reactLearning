8a8f7ec39761bf1b0b1639edc916b7833be79833
adityaashuklaa <adityaa.shuklaa07@gmail.com>
2026-01-22 00:07:24 +0530

    Created express endpoints

--- bridgeDevelop/backend/server.js
+++ bridgeDevelop/backend/server.js
@@ -6,4 +6,4 @@ app.get('/', (req, res) => {
     res.send('Server is ready!')
 })
 
-const port = process.env.port || 3000;
+const port = process.env.port || 3000; // This states if there is an env file then the port will be taken from that file otherwise for fallback there is local port also being provided, but when the application goes to prod mode, than the env port takes the priority, if it's not present than the application won't run.
